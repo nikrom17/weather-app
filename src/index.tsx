619d580715725584a8a15ee3bfb695fa300dabe7
Nik <nikrom17@gmail.com>
2021-02-03 20:59:39 -0600

    updatte fileenames

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import thunk from "redux-thunk";
-import App from "./app/app";
+import App from "./app/App";
 import { Provider } from "react-redux";
 import { createStore, applyMiddleware, compose } from "redux";
 import { rootReducer } from "./redux/rootReducer";
